38124e72c7f69a8aa48837f9965a20eb36d56b26
Odium78 <davidlancepacheco@gmail.com>
2023-03-13 23:38:57 +0800

    new game

---
+++ wizard game/source/LevelState.hx
@@ -0,0 +1,88 @@
+package;
+
+import flixel.FlxCamera.FlxCameraFollowStyle;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.addons.editors.tiled.TiledMap;
+import flixel.addons.editors.tiled.TiledObjectLayer;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxGroup;
+import flixel.util.FlxCollision;
+
+class LevelState extends FlxState
+{
+	var levelBounds:FlxGroup;
+	var player:Player;
+
+	// final platformsGroup = new FlxTypedGroup<FlxSprite>();
+	// final starsGroup = new FlxTypedGroup<FlxSprite>();
+	// var hud:Hud;
+	// var totalStars = 0;
+	// public var nextLevel:Class<LevelState>;
+
+	function createLevel(playerPos:{x:Int, y:Int})
+	{
+		bgColor = 0xffcccccc;
+		levelBounds = FlxCollision.createCameraWall(FlxG.camera, true, 1);
+		player = new Player(playerPos.x, playerPos.y);
+		add(player);
+
+		FlxG.camera.follow(player, LOCKON);
+	}
+
+	// function createLevel(levelName:String, playerPos:{x:Int, y:Int})
+	// {
+	// 	final map = new TiledMap('assets/data/$levelName.obg');
+	// 	createPlatforms(map);
+	// 	createStars(map);
+	// 	player = new Player(playerPos.x, playerPos.y);
+	// 	add(player);
+	// 	levelBounds = FlxCollision.createCameraWall(FlxG.camera, true, 1);
+	// 	hud = new Hud();
+	// 	add(hud);
+	// }
+	// function createStars(map:TiledMap)
+	// {
+	// 	final starsLayer:TiledObjectLayer = cast(map.getLayer("stars"));
+	// 	totalStars = starsLayer.objects.length;
+	// 	for (stars in starsLayer.objects)
+	// 	{
+	// 		final starsSprites = new FlxSprite(stars.x, (stars.y - stars.height));
+	// 		starsSprites.loadGraphic("assets/images/star.png", false, 400, 32);
+	// 		starsSprites.immovable = true;
+	// 		starsGroup.add(starsSprites);
+	// 	}
+	// 	add(starsGroup);
+	// }
+	// function createPlatforms(map:TiledMap)
+	// {
+	// 	final platformsLayer:TiledObjectLayer = cast(map.getLayer("platforms"));
+	// 	for (platform in platformsLayer.objects)
+	// 	{
+	// 		final platformSprites = new FlxSprite(platform.x, (platform.y - platform.height));
+	// 		platformSprites.loadGraphic("assets/images/platform.png", false, 400, 32);
+	// 		platformSprites.immovable = true;
+	// 		platformsGroup.add(platformSprites);
+	// 	}
+	// 	add(platformsGroup);
+	// }
+	// function collectStar(_, star:FlxSprite)
+	// {
+	// 	hud.incrementScore();
+	// 	star.kill();
+	// 	if (hud.score == totalStars)
+	// 	{
+	// 		final levelComplete = new LevelComplete(nextLevel);
+	// 		openSubState(levelComplete);
+	// 	}
+	// }
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		FlxG.collide(player, levelBounds);
+		// FlxG.collide(player, platformsGroup);
+		// FlxG.overlap(player, starsGroup, collectStar);
+	}
+}
---
+++ wizard game/source/Player.hx
@@ -0,0 +1,74 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+
+class Player extends FlxSprite
+{
+	final SPEED:Int = 100;
+	final GRAVITY:Int = 600;
+
+	public function new(x:Int = 0, y:Int = 0)
+	{
+		super(x, y);
+		loadGraphic("assets/images/wizard.png", true, 24, 24);
+		drag.x = SPEED * 4;
+
+		animation.add("run", [0, 1, 2], 10);
+		animation.add("idle", [0], 12, false);
+
+		setFacingFlip(LEFT, true, false);
+		setFacingFlip(RIGHT, false, false);
+
+		acceleration.y = GRAVITY;
+	}
+
+	function movement()
+	{
+		final left = FlxG.keys.anyPressed([LEFT, A]);
+		final right = FlxG.keys.anyPressed([RIGHT, D]);
+
+		if ((left || right) && !(left && right))
+		{
+			animation.play("run");
+		}
+		else
+		{
+			animation.play("idle");
+		}
+
+		if (left && right)
+		{
+			velocity.x = 0;
+			animation.play("idle");
+		}
+		else if (left)
+		{
+			velocity.x = -SPEED;
+			facing = LEFT;
+		}
+		else if (right)
+		{
+			velocity.x = SPEED;
+			facing = RIGHT;
+		}
+	}
+
+	function jumping()
+	{
+		final jump = FlxG.keys.anyPressed([UP, W, SPACE]);
+
+		if (jump && isTouching(FLOOR))
+		{
+			velocity.y = -GRAVITY / 2;
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		jumping();
+		super.update(elapsed);
+		movement();
+	}
+}
